e04e68bc05ddd874fc7e46628a1689e351327134
Alex <pavlof01@bk.ru>
2020-04-21 01:36:18 +0800

    created SubdivisionInfo component and story + created demo layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,36 @@
 import React from 'react'
 
-import Menu from './components/Dropdown'
-import Button from './components/Button'
 import HeaderMenu from './components/HeaderMenu'
-import { Img, Box } from 'styles'
+import SubdivisionInfo from './components/SubdivisionInfo'
+import { Box } from 'styles'
 
 const App = () => {
   return (
     <Box>
       <HeaderMenu />
+      <Box my={50} />
+      <Box borderRadius={16} boxShadow={10} m={24}>
+        <SubdivisionInfo
+          title="Управление информационных систем"
+          owner={{ name: 'Иванова Виктория', photo: 'URL' }}
+          numOfServices={7}
+        />
+        <SubdivisionInfo
+          title="Управление информационных систем"
+          owner={{ name: 'Иванова Виктория', photo: 'URL' }}
+          numOfServices={7}
+        />
+        <SubdivisionInfo
+          title="Управление информационных систем"
+          owner={{ name: 'Иванова Виктория', photo: 'URL' }}
+          numOfServices={7}
+        />
+        <SubdivisionInfo
+          title="Управление информационных систем"
+          owner={{ name: 'Иванова Виктория', photo: 'URL' }}
+          numOfServices={7}
+        />
+      </Box>
     </Box>
   )
 }
